4be45426c3c201aa0c7f1f352096252b74658f90
Mireia Olivé <mirisfu@gmail.com>
2023-04-21 17:45:30 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const jokeButton: HTMLButtonElement = document.querySelector("getJoke")!;
+const jokeButton: HTMLElement | null = document.querySelector("getJoke")!;
 jokeButton.addEventListener("click", nextJoke);
 
 async function getData() {
@@ -15,7 +15,8 @@ function nextJoke(): void {
     getData()
         .then((data: { joke: string }) => {
             const joke: string = data.joke;
-            const jokeHolder: HTMLElement = document.getElementById("joke")!;
+            const jokeHolder: HTMLElement | null =
+                document.getElementById("joke")!;
             jokeHolder.textContent = joke;
             // Guardar el chiste actual
             let currentJoke: string;
